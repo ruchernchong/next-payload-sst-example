5aaf12149db5241084e149857eac49085cdeea7b
Ru Chern Chong <hello@ruchern.xyz>
2023-10-22 02:39:44 +0800

    Update secrets in SST config

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,5 @@
 import { SSTConfig } from "sst";
-import { NextjsSite } from "sst/constructs";
+import { Config, NextjsSite } from "sst/constructs";
 
 export default {
   config(_input) {
@@ -9,19 +9,17 @@ export default {
     };
   },
   stacks(app) {
+    app.addDefaultFunctionEnv({
+      DATABASE_URI: process.env.DATABASE_URI as string,
+      PAYLOAD_SECRET: process.env.PAYLOAD_SECRET as string,
+      PAYLOAD_CONFIG_PATH: process.env.PAYLOAD_SECRET as string,
+    });
+
     app.stack(function Site({ stack }) {
       const site = new NextjsSite(stack, "site", {
         customDomain: {
           domainName: "next-payload-sst-example.ruchern.xyz",
           hostedZone: "ruchern.xyz",
-        },
-      });
-
-      app.setDefaultFunctionProps({
-        environment: {
-          DATABASE_URI: process.env.DATABASE_URI as string,
-          PAYLOAD_SECRET: process.env.PAYLOAD_SECRET as string,
-          PAYLOAD_CONFIG_PATH: process.env.PAYLOAD_SECRET as string,
         },
       });
 
